22c8eea18fdfa61561ae5555d63548426f234583
sajithlakshan <57319180+sajithlakshan@users.noreply.github.com>
2021-12-15 15:34:46 +0530

    Add files via upload

---
+++ PHP/Forms Methods/method="post"/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+</head>
+<body>
+    <form method="post" action="LOGIN.php">
+        NAME: <input type="text" name="fname">
+        <br>
+        <input type="submit">
+
+    </form>
+</body>
+</html>
