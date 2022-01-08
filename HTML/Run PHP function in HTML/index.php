5e8479808ffce147c717dfbe1e65c4b60e2de00d
sajithlakshan <57319180+sajithlakshan@users.noreply.github.com>
2022-01-08 19:50:58 +0530

    Add files via upload

---
+++ HTML/Run PHP function in HTML/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button onclick="clickMe()"> Click </button>
+    <script>
+    function clickMe(){
+    var result ="<?php php_func(); ?>"
+    document.write(result);
+    }
+    </script>
+
+    <?php
+        function php_func(){
+        echo "Stay Safe";
+    }
+    ?>
+</body>
+</html>
